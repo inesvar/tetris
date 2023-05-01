1822b8992c9d44af982010e8c80b57cb967566e4
inesvar <90340779+inesvar@users.noreply.github.com>
2023-05-01 16:23:05 +0200

    Collision trait for Block

--- src/block.rs
+++ src/block.rs
@@ -1,11 +1,11 @@
-use crate::point::{Point, Transformable};
+use crate::assets::Assets;
 use crate::assets::TetrisColor;
-use crate::settings::{BLOCK_SIZE};
+use crate::point::{Point, Transformable};
+use crate::settings::BLOCK_SIZE;
 use graphics::math::{margin_rectangle, Matrix2d, Scalar};
 use graphics::types::Color;
 use graphics::{rectangle, Context, DrawState, Image};
 use opengl_graphics::GlGraphics;
-use crate::assets::Assets;
 
 #[derive(Clone, Copy)]
 pub struct Block {
@@ -21,11 +21,17 @@ impl Block {
         }
     }
 
-    /*pub fn set_position(mut self, x: i8, y: i8) {
-        Point::set(&mut self.position, x, y);
-    }*/
+    pub fn set_position(mut self, point: Point) {
+        self.position = point;
+    }
 
-    pub fn render(&self, transform: Matrix2d, draw_state: &DrawState, gl: &mut GlGraphics, assets: &Assets) {
+    pub fn render(
+        &self,
+        transform: Matrix2d,
+        draw_state: &DrawState,
+        gl: &mut GlGraphics,
+        assets: &Assets,
+    ) {
         let dims = rectangle::square(
             self.position.x as Scalar * BLOCK_SIZE,
             self.position.y as Scalar * BLOCK_SIZE,
@@ -67,5 +73,108 @@ impl Transformable for Block {
 
     fn rotate_counterclockwise(&mut self, other: &Point) {
         self.position.rotate_counterclockwise(other);
+    }
+}
+
+pub enum NewBlock {
+    Error,
+    Success(Block),
+}
+
+pub trait Collision {
+    fn fall(&mut self, matrix: &Vec<Vec<Option<Block>>>) -> NewBlock;
+    fn right(&mut self, matrix: &Vec<Vec<Option<Block>>>) -> NewBlock;
+    fn left(&mut self, matrix: &Vec<Vec<Option<Block>>>) -> NewBlock;
+    fn turn_clockwise(&mut self, other: &Point, matrix: &Vec<Vec<Option<Block>>>) -> NewBlock;
+    fn turn_counterclockwise(
+        &mut self,
+        other: &Point,
+        matrix: &Vec<Vec<Option<Block>>>,
+    ) -> NewBlock;
+}
+
+impl Collision for Block {
+    fn fall(&mut self, matrix: &Vec<Vec<Option<Block>>>) -> NewBlock {
+        if self.position.y <= 0 {
+            NewBlock::Error
+        } else {
+            match matrix[(self.position.y - 1) as usize][self.position.x as usize] {
+                Some(_) => NewBlock::Error,
+                None => {
+                    self.go_down();
+                    NewBlock::Success(*self)
+                }
+            }
+        }
+    }
+
+    fn left(&mut self, matrix: &Vec<Vec<Option<Block>>>) -> NewBlock {
+        if self.position.x <= 0 {
+            NewBlock::Error
+        } else {
+            match matrix[self.position.y as usize][(self.position.x - 1) as usize] {
+                Some(_) => NewBlock::Error,
+                None => {
+                    self.go_left();
+                    NewBlock::Success(*self)
+                }
+            }
+        }
+    }
+
+    fn right(&mut self, matrix: &Vec<Vec<Option<Block>>>) -> NewBlock {
+        if self.position.x as usize >= matrix[0].len() - 1 {
+            NewBlock::Error
+        } else {
+            match matrix[self.position.y as usize][(self.position.x - 1) as usize] {
+                Some(_) => NewBlock::Error,
+                None => {
+                    self.go_right();
+                    NewBlock::Success(*self)
+                }
+            }
+        }
+    }
+
+    fn turn_clockwise(&mut self, other: &Point, matrix: &Vec<Vec<Option<Block>>>) -> NewBlock {
+        let mut copy = self.position;
+        copy.rotate_clockwise(other);
+        match (copy.x, copy.y) {
+            (x, y) if x < 0 || y < 0 => {
+                return NewBlock::Error;
+            }
+            (x, y) if x as usize >= matrix[0].len() - 1 || y as usize >= matrix.len() - 1 => {
+                return NewBlock::Error;
+            }
+            _ => {}
+        }
+        match matrix[copy.y as usize][copy.x as usize] {
+            Some(_) => NewBlock::Error,
+            None => {
+                self.set_position(copy);
+                NewBlock::Success(*self)
+            }
+        }
+    }
+
+    fn turn_counterclockwise(&mut self, other: &Point, matrix: &Vec<Vec<Option<Block>>>) -> NewBlock {
+        let mut copy = self.position;
+        copy.rotate_counterclockwise(other);
+        match (copy.x, copy.y) {
+            (x, y) if x < 0 || y < 0 => {
+                return NewBlock::Error;
+            }
+            (x, y) if x as usize >= matrix[0].len() - 1 || y as usize >= matrix.len() - 1 => {
+                return NewBlock::Error;
+            }
+            _ => {}
+        }
+        match matrix[copy.y as usize][copy.x as usize] {
+            Some(_) => NewBlock::Error,
+            None => {
+                self.set_position(copy);
+                NewBlock::Success(*self)
+            }
+        }
     }
 }
