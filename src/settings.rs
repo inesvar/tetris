282d31cf3f628a6d103fed291ac58707069c7544
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-05-01 16:58:01 +0200

    separated active_tetromino from grid

--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,4 @@
-use graphics::color::GRAY;
+use graphics::color;
 
 pub static DEFAULT_WINDOW_WIDTH: u32 = 500;
 pub static DEFAULT_WINDOW_HEIGHT: u32 = 600;
@@ -6,4 +6,5 @@ pub static DEFAULT_WINDOW_HEIGHT: u32 = 600;
 pub static BLOCK_SIZE: f64 = 25.0;
 pub static GRID_THICKNESS: f64 = 0.2;
 
-pub static GRID_COLOR: graphics::types::Color = GRAY;
+pub static BG_COLOR: graphics::types::Color = color::BLACK;
+pub static GRID_COLOR: graphics::types::Color = color::GRAY;
