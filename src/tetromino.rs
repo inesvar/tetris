203d13494f1a44684ee78937f827962bb05e34df
inesvar <90340779+inesvar@users.noreply.github.com>
2023-05-03 13:33:54 +0200

    Added keyboard, the tetromino move on pressed keys
    - added new methods for all tetromino types
    - changed rotation for O type (I type still unfixed)

--- src/tetromino.rs
+++ src/tetromino.rs
@@ -7,8 +7,20 @@ use opengl_graphics::GlGraphics;
 use crate::assets::Assets;
 use crate::assets::TetrisColor;
 
+#[derive(PartialEq)]
+pub enum TetrominoKind {
+    I,
+    O,
+    T,
+    S,
+    Z,
+    J,
+    L,
+}
+
 pub struct Tetromino {
     color: TetrisColor,
+    kind: TetrominoKind,
     center: Point,
     blocks: [Block; 4],
     rotation_status: Rotation,
@@ -22,13 +34,14 @@ pub enum Rotation {
 }
 
 impl Tetromino {
-    pub fn new(color: TetrisColor, positions: &mut [i8]) -> Self {
+    pub fn new(color: TetrisColor, kind: TetrominoKind, positions: &mut [i8]) -> Self {
         for i in 1..5 {
             positions[2 * i] += positions[0];
             positions[2 * i + 1] += positions[1];
         }
         Tetromino {
             color,
+            kind,
             center: Point::new(positions[0], positions[1]),
             blocks: [
                 Block::new(color, positions[2], positions[3]),
@@ -38,6 +51,62 @@ impl Tetromino {
             ],
             rotation_status: Rotation::R0,
         }
+    }
+
+    pub fn new_T() -> Self {
+        Tetromino::new(
+            TetrisColor::PURPLE,
+            TetrominoKind::T,
+            &mut [5, 2, 1, 0, -1, 0, 0, 0, 0, -1]
+        )
+    }
+
+    pub fn new_S() -> Self {
+        Tetromino::new(
+            TetrisColor::GREEN,
+            TetrominoKind::S,
+            &mut [5, 2, -1, 0, 0, 0, 0, -1, 1, -1]
+        )
+    }
+
+    pub fn new_Z() -> Self {
+        Tetromino::new(
+            TetrisColor::RED,
+            TetrominoKind::Z,
+            &mut [5, 2, -1, -1, 0, -1, 0, 0, 1, 0]
+        )
+    }
+
+    pub fn new_L() -> Self {
+        Tetromino::new(
+            TetrisColor::ORANGE,
+            TetrominoKind::L,
+            &mut [5, 2, -1, 0, 0, 0, 1, 0, 1, -1]
+        )
+    }
+
+    pub fn new_J() -> Self {
+        Tetromino::new(
+            TetrisColor::BLUE,
+            TetrominoKind::J,
+            &mut [5, 2, -1, -1, -1, 0, 0, 0, 1, 0]
+        )
+    }
+
+    pub fn new_O() -> Self {
+        Tetromino::new(
+            TetrisColor::YELLOW,
+            TetrominoKind::O,
+            &mut [5, 2, -1, -1, -1, 0, 0, -1, 0, 0]
+        )
+    }
+
+    pub fn new_I() -> Self {
+        Tetromino::new(
+            TetrisColor::CYAN,
+            TetrominoKind::I,
+            &mut [5, 2, -2, 0, -1, 0, 0, 0, 1, 0]
+        )
     }
 }
 
@@ -116,6 +185,9 @@ impl Tetromino {
     }
 
     pub fn turn_clockwise(&mut self, matrix: &Vec<Vec<Option<Block>>>) -> NewTetromino {
+        if self.kind == TetrominoKind::O {
+            return NewTetromino::Success;
+        };
         let mut new_blocks = vec![];
         for i in 0..4 {
             match self.blocks[i].turn_clockwise(&self.center, matrix) {
@@ -132,6 +204,9 @@ impl Tetromino {
     }
 
     pub fn turn_counterclockwise(&mut self, matrix: &Vec<Vec<Option<Block>>>) -> NewTetromino {
+        if self.kind == TetrominoKind::O {
+            return NewTetromino::Success;
+        };
         let mut new_blocks = vec![];
         for i in 0..4 {
             match self.blocks[i].turn_counterclockwise(&self.center, matrix) {
