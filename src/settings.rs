112b65250d9a6445e51b9407c2cba2ab575e8179
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-05-04 14:55:52 +0200

    holding is now working

--- src/settings.rs
+++ src/settings.rs
@@ -10,9 +10,11 @@ pub static GRID_THICKNESS: f64 = 0.5;
 pub static BG_COLOR: graphics::types::Color = color::BLACK;
 pub static GRID_COLOR: graphics::types::Color = color::GRAY;
 
-pub static FALL_KEYS: [Key; 2] = [Key::Down, Key::C];
+pub static FALL_KEYS: [Key; 2] = [Key::Down, Key::S];
 pub static HARD_DROP_KEYS: [Key; 1] = [Key::Space];
-pub static RIGHT_KEYS: [Key; 2] = [Key::Right, Key::V];
-pub static LEFT_KEYS: [Key; 2] = [Key::Left, Key::X];
-pub static ROTATE_CLOCKWISE_KEYS: [Key; 2] = [Key::Up, Key::F];
-pub static ROTATE_COUNTERCLOCKWISE_KEYS: [Key; 2] = [Key::NumPad3, Key::D];
+pub static RIGHT_KEYS: [Key; 2] = [Key::Right, Key::D];
+pub static LEFT_KEYS: [Key; 2] = [Key::Left, Key::Q];
+pub static ROTATE_CLOCKWISE_KEYS: [Key; 2] = [Key::Up, Key::E];
+pub static ROTATE_COUNTERCLOCKWISE_KEYS: [Key; 2] = [Key::NumPad3, Key::A];
+
+pub static HOLD_TETROMINO_KEYS: [Key; 1] = [Key::C];
