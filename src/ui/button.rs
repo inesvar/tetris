f751f625fd8087f33e58758fe0594de81c6d69eb
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-05-13 20:03:44 +0200

    starting small ui module

---
+++ src/ui/button.rs
@@ -0,0 +1,66 @@
+use graphics::{color, Context, rectangle};
+use opengl_graphics::GlGraphics;
+use piston::RenderArgs;
+use crate::assets::Assets;
+use crate::graphics::Transformed;
+
+pub struct Button {
+    pub x: i8,
+    pub y: i8,
+    pub width: i8,
+    pub height: i8,
+    pub text: String,
+    pub pressed: bool,
+}
+
+
+impl Button {
+    pub fn new(x: i8, y: i8, width: i8, height: i8, text: String) -> Self {
+        Button {
+            x,
+            y,
+            width,
+            height,
+            text,
+            pressed: false,
+        }
+    }
+
+    pub fn is_clicked(&self, x: i8, y: i8) -> bool {
+        if x >= self.x && x <= self.x + self.width && y >= self.y && y <= self.y + self.height {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_pressed(&self) -> bool {
+        self.pressed
+    }
+
+    pub fn render(&mut self, args: &RenderArgs, ctx: &Context, gl: &mut GlGraphics, assets: &mut Assets) {
+        let dims = rectangle::rectangle_by_corners(
+            self.x as f64,
+            self.y as f64,
+            (self.x + self.width) as f64,
+            (self.y + self.height) as f64,
+        );
+        let button = graphics::Rectangle::new([0.0, 0.0, 0.0, 0.5]);
+
+        button.draw(dims, &ctx.draw_state, ctx.transform, gl);
+
+        let text_transform = ctx.transform.trans(
+            self.x as f64 + self.width as f64 / 2.0 - 8.0,
+            self.y as f64 + self.height as f64 / 2.0 + 8.0,
+        );
+        graphics::text::Text::new_color(color::WHITE, 16)
+            .draw(
+                "T",
+                &mut assets.main_font,
+                &ctx.draw_state,
+                text_transform,
+                gl,
+            )
+            .unwrap();
+    }
+}
