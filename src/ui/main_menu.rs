7607bbca4540ee9040280eca4e3b5975a617f956
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-05-23 14:24:58 +0200

    created main menu logic

---
+++ src/ui/main_menu.rs
@@ -0,0 +1,20 @@
+use crate::settings::{DEFAULT_WINDOW_HEIGHT, DEFAULT_WINDOW_WIDTH};
+use crate::ui::button::Button;
+
+pub struct MainMenu {
+    pub create_single_player_game_button: Button,
+}
+
+impl MainMenu {
+    pub fn new() -> MainMenu {
+        MainMenu {
+            create_single_player_game_button: Button::new(
+                DEFAULT_WINDOW_WIDTH as f64 / 2.0,
+                DEFAULT_WINDOW_HEIGHT as f64 / 2.0,
+                300.0,
+                50.0,
+                String::from("Create new single-player game"),
+            )
+        }
+    }
+}
