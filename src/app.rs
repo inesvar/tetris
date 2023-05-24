130197539ead428f85169903e3f94f0853194b5e
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-05-24 10:37:09 +0200

    fixed merge

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use std::cell::RefCell;
 use std::ops::{Deref, DerefMut};
-use crate::local_player::{KeyPress, LocalPlayer, Player};
+use crate::local_player::{KeyPress, LocalPlayer};
 
 use crate::remote_player::RemotePlayer;
 use crate::settings::*;
