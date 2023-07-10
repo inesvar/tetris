f2ae040520bb42edb403d7d3b3d919a8e474dd8a
inesvar <90340779+inesvar@users.noreply.github.com>
2023-07-10 17:38:25 +0200

    Removed keybindings from settings.rs as they're not used

--- src/settings.rs
+++ src/settings.rs
@@ -5,9 +5,6 @@ use piston::Key;
 // Change this to OpenGL::V2_1 if not working.
 pub static OPENGL_VERSION: OpenGL = OpenGL::V4_5;
 
-pub static NB_COLUMNS: u32 = 10;
-pub static NB_ROWS: u32 = 22;
-
 pub static DEFAULT_WINDOW_WIDTH: u32 = 650;
 pub static DEFAULT_WINDOW_HEIGHT: u32 = 700;
 
@@ -16,15 +13,6 @@ pub static GRID_THICKNESS: f64 = 0.5;
 
 pub static BG_COLOR: graphics::types::Color = color::BLACK;
 pub static GRID_COLOR: graphics::types::Color = color::GRAY;
-
-pub static FALL_KEYS: [Key; 2] = [Key::Down, Key::S];
-pub static HARD_DROP_KEYS: [Key; 1] = [Key::Space];
-pub static RIGHT_KEYS: [Key; 2] = [Key::Right, Key::D];
-pub static LEFT_KEYS: [Key; 2] = [Key::Left, Key::Q];
-pub static ROTATE_CLOCKWISE_KEYS: [Key; 2] = [Key::Up, Key::E];
-pub static ROTATE_COUNTERCLOCKWISE_KEYS: [Key; 2] = [Key::NumPad3, Key::A];
-pub static RESTART_KEYS: [Key; 1] = [Key::R];
-pub static HOLD_TETROMINO_KEYS: [Key; 1] = [Key::C];
 
 pub static KEY_REPEAT_DELAY: u64 = 20;
 
