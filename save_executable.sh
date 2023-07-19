440288819fedc17b1fc235ad90d0d67cb1f1b38d
inesvar <90340779+inesvar@users.noreply.github.com>
2023-07-19 17:52:40 +0200

    Corrected script

--- save_executable.sh
+++ save_executable.sh
@@ -3,6 +3,7 @@ folderName="tetrisApp"
 
 path=~/"$folderName"
 cargo build
+rm -rf "$path"
 mkdir "$path";
-cp ./target/release/tetris "$path"
+cp ./target/debug/tetris "$path"
 cp -r ./src/assets "$path"
