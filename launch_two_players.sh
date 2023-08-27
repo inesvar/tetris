d7cd9331c38a9952358cd05a269f732486f31c8b
inesvar <90340779+inesvar@users.noreply.github.com>
2023-08-27 15:31:36 +0200

    The two useful IPs are now read from text files instead of being hard coded

---
+++ launch_two_players.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+folderName="tetrisApp" 
+#
+# this folder has to contain :
+#   - the assets folder
+#   - a file named "remote_ip.txt" with the same content than "local_ip.txt" from this repo
+#   - a file named "local_ip.txt" with the same content than "remote_ip.txt" from this repo
+
+path=~/"$folderName"
+cargo build
+# erases the executable
+rm -rf "$path"/tetris
+# copies the current executable
+cp ./target/debug/tetris "$path"
+
+#launch one player
+cargo run -- --two-remote&
+
+cd ~/tetrisApp
+
+#then the second
+./tetris --two-remote
