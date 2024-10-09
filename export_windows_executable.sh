2516fd30fa67adca3d22a43b368b3b317085422d
inesvar <inesvar 90340779+inesvar@users.noreply.github.com>
2024-10-09 21:30:42 +0200

    Rename scripts to export tetris executable

--- export_windows_executable.sh
+++ export_windows_executable.sh
@@ -2,10 +2,8 @@
 folderName="tetrisWindowsApp" 
 path=~/"$folderName"
 
-# building the executable
 cargo build --release --target x86_64-pc-windows-gnu
 
-# copies the current executable and assets to "~/tetrisApp"
 mkdir "$path"
 cp ./target/x86_64-pc-windows-gnu/release/tetris.exe "$path"
 cp -r ./src/assets "$path"
