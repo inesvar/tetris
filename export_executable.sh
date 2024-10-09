9ba9ffdfd888539a63fdd0aee691f8b5d1950c43
inesvar <90340779+inesvar@users.noreply.github.com>
2024-10-09 21:30:42 +0200

    Rename scripts to export tetris executable

--- export_executable.sh
+++ export_executable.sh
@@ -2,10 +2,8 @@
 folderName="tetrisApp" 
 path=~/"$folderName"
 
-# building the executable
 cargo build --release
 
-# copies the current executable and assets to "~/tetrisApp"
 mkdir "$path"
 cp ./target/release/tetris "$path"
 cp -r ./src/assets "$path"
