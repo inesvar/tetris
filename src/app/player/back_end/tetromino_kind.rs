0c959cba7f454b147550fa35779395ad20ae28a8
inesvar <90340779+inesvar@users.noreply.github.com>
2025-05-22 19:35:48 +0200

    Reading tetromino_kind

--- src/app/player/back_end/tetromino_kind.rs
+++ src/app/player/back_end/tetromino_kind.rs
@@ -25,6 +25,25 @@ const RIGHT_TO_UP_WALL_KICKS: [Position; 5] = const_map!(UP_TO_RIGHT_WALL_KICKS,
 const DOWN_TO_LEFT_WALL_KICKS: [Position; 5] = const_map!(UP_TO_RIGHT_WALL_KICKS, mirror_x);
 const LEFT_TO_DOWN_WALL_KICKS: [Position; 5] = const_map!(UP_TO_RIGHT_WALL_KICKS, mirror_y);
 
+const UP_TO_RIGHT_I_WALL_KICKS: [Position; 5] = [
+    Position::new(0, 0),
+    Position::new(-2, 0),
+    Position::new(1, 0),
+    Position::new(-2, -1),
+    Position::new(1, 2),
+];
+
+const RIGHT_TO_BOTTOM_I_WALL_KICKS: [Position; 5] = [
+    Position::new(0, 0),
+    Position::new(-1, 0),
+    Position::new(2, 0),
+    Position::new(-1, 2),
+    Position::new(2, -1),
+];
+
+const RIGHT_TO_UP_I_WALL_KICKS: [Position; 5] = const_map!(UP_TO_RIGHT_I_WALL_KICKS, neg);
+const BOTTOM_TO_RIGHT_I_WALL_KICKS: [Position; 5] = const_map!(RIGHT_TO_BOTTOM_I_WALL_KICKS, neg);
+
 const fn init(x: i32, y: i32) -> Position {
     match (x, y) {
         (x @ 0..4, y @ 0..4) => Position::new(3 + x, y),
@@ -66,8 +85,6 @@ impl TetrominoKind {
         *self != TetrominoKind::I
     }
 
-    // the rotation of the I and O should be tweaked, for better clarity. hummm yes but it's practical...
-    // BUT before changing everything, unit tests need to be written ! yay !
     /// Return an array of the 5 SRS wall-kick translations.
     pub(super) fn wall_kick_translations(
         &self,
@@ -78,71 +95,23 @@ impl TetrominoKind {
         match self {
             // since the O piece doesn't even rotate
             TetrominoKind::O => unreachable!(),
-            // the additional calculation for the I piece is to compensate for the fact
-            // that its rotational center is actually between two blocks
-            // and not on a block like in the code
-            // (the true position of the initial center is (4.5, 1) and not (4, 1) see line 32.)
-            // (its due to the fact that the I piece doesn't have a 3x3 bounding box like the other rotating pieces)
-            TetrominoKind::I => match (rotation_status, rtype) {
-                (Direction::Up, RotationType::Clockwise) => [
-                    Position::new(0, 0),
-                    Position::new(-2, 0),
-                    Position::new(1, 0),
-                    Position::new(-2, -1),
-                    Position::new(1, 2),
-                ],
-                (Direction::Right, RotationType::Counterclockwise) => [
-                    Position::new(0, 0),
-                    Position::new(2, 0),
-                    Position::new(-1, 0),
-                    Position::new(2, 1),
-                    Position::new(-1, -2), // neg neg
-                ],
-                (Direction::Right, RotationType::Clockwise) => [
-                    Position::new(0, 0),
-                    Position::new(-1, 0),
-                    Position::new(2, 0),
-                    Position::new(-1, 2),
-                    Position::new(2, -1),
-                ],
-                (Direction::Down, RotationType::Counterclockwise) => [
-                    Position::new(0, 0),
-                    Position::new(1, 0),
-                    Position::new(-2, 0),
-                    Position::new(1, -2),
-                    Position::new(-2, 1),
-                ],
-                (Direction::Down, RotationType::Clockwise) => [
-                    Position::new(0, 0),
-                    Position::new(2, 0),
-                    Position::new(-1, 0),
-                    Position::new(2, 1),
-                    Position::new(-1, -2),
-                ],
-                (Direction::Left, RotationType::Counterclockwise) => [
-                    Position::new(0, 0),
-                    Position::new(-2, 0),
-                    Position::new(1, 0),
-                    Position::new(-2, -1),
-                    Position::new(1, 2),
-                ],
-                (Direction::Left, RotationType::Clockwise) => [
-                    Position::new(0, 0),
-                    Position::new(1, 0),
-                    Position::new(-2, 0),
-                    Position::new(1, -2),
-                    Position::new(-2, 1),
-                ],
-                (Direction::Up, RotationType::Counterclockwise) => [
-                    Position::new(0, 0),
-                    Position::new(-1, 0),
-                    Position::new(2, 0),
-                    Position::new(-1, 2),
-                    Position::new(2, -1),
-                ],
-                (_, _) => todo!(),
-            },
+            TetrominoKind::I => Self::i_wall_kick_translations(rtype, rotation_status),
             _ => Self::generic_wall_kick_translations(rtype, rotation_status),
+        }
+    }
+
+    fn i_wall_kick_translations(rtype: RotationType, rotation_status: Direction) -> [Position; 5] {
+        // cf https://tetris.fandom.com/wiki/SRS#Wall_Kicks
+        match (rotation_status, rtype) {
+            (Direction::Up, RotationType::Clockwise) => UP_TO_RIGHT_I_WALL_KICKS,
+            (Direction::Right, RotationType::Counterclockwise) => RIGHT_TO_UP_I_WALL_KICKS,
+            (Direction::Right, RotationType::Clockwise) => RIGHT_TO_BOTTOM_I_WALL_KICKS,
+            (Direction::Down, RotationType::Counterclockwise) => BOTTOM_TO_RIGHT_I_WALL_KICKS,
+            (Direction::Down, RotationType::Clockwise) => RIGHT_TO_UP_I_WALL_KICKS,
+            (Direction::Left, RotationType::Counterclockwise) => UP_TO_RIGHT_I_WALL_KICKS,
+            (Direction::Left, RotationType::Clockwise) => BOTTOM_TO_RIGHT_I_WALL_KICKS,
+            (Direction::Up, RotationType::Counterclockwise) => RIGHT_TO_BOTTOM_I_WALL_KICKS,
+            (_, _) => todo!(),
         }
     }
 
@@ -263,6 +232,58 @@ mod tests {
                 Direction::Up
             ),
             positions!((0, 0), (1, 0), (1, 1), (0, -2), (1, -2))
+        );
+    }
+
+    #[test]
+    fn i_wall_kicks_are_correct() {
+        assert_eq!(
+            TetrominoKind::i_wall_kick_translations(RotationType::Clockwise, Direction::Up),
+            positions!((0, 0), (-2, 0), (1, 0), (-2, -1), (1, 2))
+        );
+
+        assert_eq!(
+            TetrominoKind::i_wall_kick_translations(
+                RotationType::Counterclockwise,
+                Direction::Right
+            ),
+            positions!((0, 0), (2, 0), (-1, 0), (2, 1), (-1, -2))
+        );
+
+        assert_eq!(
+            TetrominoKind::i_wall_kick_translations(RotationType::Clockwise, Direction::Right),
+            positions!((0, 0), (-1, 0), (2, 0), (-1, 2), (2, -1))
+        );
+
+        assert_eq!(
+            TetrominoKind::i_wall_kick_translations(
+                RotationType::Counterclockwise,
+                Direction::Down
+            ),
+            positions!((0, 0), (1, 0), (-2, 0), (1, -2), (-2, 1))
+        );
+
+        assert_eq!(
+            TetrominoKind::i_wall_kick_translations(RotationType::Clockwise, Direction::Down),
+            positions!((0, 0), (2, 0), (-1, 0), (2, 1), (-1, -2))
+        );
+
+        assert_eq!(
+            TetrominoKind::i_wall_kick_translations(
+                RotationType::Counterclockwise,
+                Direction::Left
+            ),
+            positions!((0, 0), (-2, 0), (1, 0), (-2, -1), (1, 2))
+        );
+
+        assert_eq!(
+            TetrominoKind::i_wall_kick_translations(RotationType::Clockwise, Direction::Left),
+            positions!((0, 0), (1, 0), (-2, 0), (1, -2), (-2, 1))
+        );
+
+        assert_eq!(
+            TetrominoKind::i_wall_kick_translations(RotationType::Counterclockwise, Direction::Up),
+            positions!((0, 0), (-1, 0), (2, 0), (-1, 2), (2, -1))
         );
     }
 }
