9342c10f2e35f911b2f643d9617df3491b505b27
inesvar <90340779+inesvar@users.noreply.github.com>
2026-01-25 14:05:43 +0100

    Make TetrominoKind private in tetris_core

--- tetris_core/src/lib.rs
+++ tetris_core/src/lib.rs
@@ -1,15 +1,16 @@
-//! Re-export `struct`s [Tetromino], [TetrisGrid], [TetrominoGenerator] and [Position] as well as `enum`s [GameOverError], [TetrominoKind], [TetrisColor] and [TetrominoMove].
+//! Re-export `struct`s [Tetromino], [TetrisGrid], [TetrominoGenerator] and [Position] as well as `enum`s [GameOverError], [TetrisColor] and [TetrominoMove].
 #![doc = simple_mermaid::mermaid!("tetris_core.mmd")]
 
 mod tetris_grid;
 mod tetromino;
 mod tetromino_generator;
 
+pub(crate) use tetromino::TetrominoKind;
+
 pub use tetris_grid::TetrisColor;
 pub use tetris_grid::NB_VISIBLE_BUFFER_ROWS;
 pub use tetris_grid::{GameOverError, TetrisGrid};
 pub use tetromino::Position;
 pub use tetromino::Tetromino;
-pub use tetromino::TetrominoKind;
 pub use tetromino::TetrominoMove;
 pub use tetromino_generator::TetrominoGenerator;
