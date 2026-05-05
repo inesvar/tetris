a926363449128c2713fb3a0b026fa107ac47ae92
inesvar <90340779+inesvar@users.noreply.github.com>
2026-05-05 19:45:21 +0200

    Add unit tests for off the floor kicks

--- core_tetris/src/tetris_player.rs
+++ core_tetris/src/tetris_player.rs
@@ -628,14 +628,8 @@ mod tests {
         let mut no_wall_kick_player = player.clone();
         no_wall_kick_player.try_apply(rotation, &mut rng, &mut garbage_rng)?;
 
-        for _ in 0..5 {
-            player.try_apply(TetrominoMove::Right.into(), &mut rng, &mut garbage_rng)?;
-            no_wall_kick_player.try_apply(
-                TetrominoMove::Right.into(),
-                &mut rng,
-                &mut garbage_rng,
-            )?;
-        }
+        while player.try_right() {}
+        while no_wall_kick_player.try_right() {}
 
         player.try_apply(rotation, &mut rng, &mut garbage_rng)?;
 
@@ -673,14 +667,52 @@ mod tests {
         let mut no_wall_kick_player = player.clone();
         no_wall_kick_player.try_apply(rotation, &mut rng, &mut garbage_rng)?;
 
-        for _ in 0..5 {
-            player.try_apply(TetrominoMove::Left.into(), &mut rng, &mut garbage_rng)?;
-            no_wall_kick_player.try_apply(
-                TetrominoMove::Left.into(),
-                &mut rng,
-                &mut garbage_rng,
-            )?;
+        while player.try_left() {}
+        while no_wall_kick_player.try_left() {}
+
+        player.try_apply(rotation, &mut rng, &mut garbage_rng)?;
+
+        assert_eq!(player.to_string(), no_wall_kick_player.to_string());
+
+        Ok(())
+    }
+
+    #[rstest]
+    /// Testing that operations "moving to the floor" and "turning 90°" are commutable,
+    /// in situations where the tetromino faces north (this encompasses all situations that
+    /// only work thanks to wall kicks).
+    fn off_the_floor(
+        #[values(
+            TetrominoKind::O,
+            TetrominoKind::T,
+            TetrominoKind::L,
+            TetrominoKind::J,
+            TetrominoKind::S,
+            TetrominoKind::Z
+        )]
+        kind: TetrominoKind,
+        #[values(TetrominoMove::Clockwise.into(), TetrominoMove::Counterclockwise.into())]
+        rotation: TetrisCommand,
+    ) -> TetrisResult {
+        let mut rng = MockRng::tetromino_cycle(&[kind]);
+        let mut garbage_rng = MockRng::right_aligned_garbage();
+        let mut player = TetrisPlayer::compact(&mut rng, BagType::NoBag);
+
+        let mut no_wall_kick_player = player.clone();
+        no_wall_kick_player.try_apply(rotation, &mut rng, &mut garbage_rng)?;
+
+        if kind != TetrominoKind::O {
+            match rotation {
+                TetrisCommand::Move(TetrominoMove::Clockwise) => no_wall_kick_player.try_left(),
+                TetrisCommand::Move(TetrominoMove::Counterclockwise) => {
+                    no_wall_kick_player.try_right()
+                }
+                _ => unreachable!(),
+            };
         }
+
+        while player.try_fall() {}
+        while no_wall_kick_player.try_fall() {}
 
         player.try_apply(rotation, &mut rng, &mut garbage_rng)?;
 
