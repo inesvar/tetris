fc76215e22c40688bab9557878cd9e965dba81d9
inesvar <90340779+inesvar@users.noreply.github.com>
2026-06-26 21:59:20 +0200

    render_app: Add render_player_score function in RenderTetrisGame

--- tetris/src/app/render_app.rs
+++ tetris/src/app/render_app.rs
@@ -7,8 +7,7 @@ use crate::settings::BG_COLOR;
 use core_tetris::render::RenderTetrisPlayer;
 use graphics::Transformed;
 use piston::RenderArgs;
-use render_tetris::Piston2dOpenGlRenderer;
-use render_tetris::{BLOCK_SIZE, DEFAULT_GRID_X};
+use render_tetris::{Piston2dOpenGlRenderer, BLOCK_SIZE, DEFAULT_GRID_X};
 use ui_tetris::{
     RenderTetrisUi, Text, DEFAULT_FONT_SIZE, DEFAULT_SCORE_TEXT_Y, DEFAULT_WINDOW_WIDTH, SILVER,
 };
@@ -16,8 +15,9 @@ use ui_tetris::{
 pub trait RenderTetrisGame: RenderTetrisPlayer + RenderTetrisUi {
     type RenderArgs;
     fn render_app(&mut self, render_args: &RenderArgs, app: &App);
-    fn render_local_player(&mut self, local_player: &LocalPlayer, state: RunningState); // TODO: put score rendering inside !!
-    fn render_remote_player(&mut self, remote_player: &RemotePlayer, state: RunningState); // TODO: put score rendering inside !!
+    fn render_local_player(&mut self, local_player: &LocalPlayer, state: RunningState);
+    fn render_remote_player(&mut self, remote_player: &RemotePlayer, state: RunningState);
+    fn render_player_score(&mut self, score: u32);
 }
 
 impl RenderTetrisGame for Piston2dOpenGlRenderer<'_> {
@@ -44,31 +44,10 @@ impl RenderTetrisGame for Piston2dOpenGlRenderer<'_> {
                 self.render_widget_manager(&app.widget_manager[0], &app.running);
 
                 for player in &app.local_players {
-                    // TODO: score rendering should eventually be in render_tetris
-                    let score_text = Text::new(
-                        format!("Score: {}", player.score()).as_str(),
-                        DEFAULT_FONT_SIZE,
-                        // the score is centered under the hold piece rectangle
-                        DEFAULT_GRID_X - 4.0 * BLOCK_SIZE, // 4.0 = 1.0 (margin between borders) + 1.0 (margin inside) + 2.0 (half TETROMINO_MAX_WIDTH)
-                        DEFAULT_SCORE_TEXT_Y,
-                        SILVER,
-                    );
-                    self.render_text(&score_text);
-
                     self.render_local_player(player, app.running);
                     self.transform = self.transform.trans(DEFAULT_WINDOW_WIDTH, 0.0);
                 }
                 for player in &app.remote_player {
-                    let score_text = Text::new(
-                        format!("Score: {}", player.score()).as_str(),
-                        DEFAULT_FONT_SIZE,
-                        // the score is centered under the hold piece rectangle
-                        DEFAULT_GRID_X - 4.0 * BLOCK_SIZE, // 4.0 = 1.0 (margin between borders) + 1.0 (margin inside) + 2.0 (half TETROMINO_MAX_WIDTH)
-                        DEFAULT_SCORE_TEXT_Y,
-                        SILVER,
-                    );
-                    self.render_text(&score_text);
-
                     self.render_remote_player(player, app.running);
                     self.transform = self.transform.trans(DEFAULT_WINDOW_WIDTH, 0.0);
                 }
@@ -80,6 +59,8 @@ impl RenderTetrisGame for Piston2dOpenGlRenderer<'_> {
     }
 
     fn render_remote_player(&mut self, remote_player: &RemotePlayer, state: RunningState) {
+        self.render_player_score(remote_player.score());
+
         if !remote_player.received_first_screen() {
             return;
         }
@@ -91,6 +72,20 @@ impl RenderTetrisGame for Piston2dOpenGlRenderer<'_> {
     }
 
     fn render_local_player(&mut self, local_player: &LocalPlayer, state: RunningState) {
+        self.render_player_score(local_player.score());
         self.display_player(local_player.get_player(), state);
+    }
+
+    fn render_player_score(&mut self, score: u32) {
+        // TODO: score rendering should eventually be in render_tetris
+        let score_text = Text::new(
+            format!("Score: {}", score).as_str(),
+            DEFAULT_FONT_SIZE,
+            // the score is centered under the hold piece rectangle
+            DEFAULT_GRID_X - 4.0 * BLOCK_SIZE, // 4.0 = 1.0 (margin between borders) + 1.0 (margin inside) + 2.0 (half TETROMINO_MAX_WIDTH)
+            DEFAULT_SCORE_TEXT_Y,
+            SILVER,
+        );
+        self.render_text(&score_text);
     }
 }
